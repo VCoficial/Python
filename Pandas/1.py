82ccb54d077950e94876358c086afafc22cc7fc1
VCoficial <ciberandresoficial@gmail.com>
2022-10-05 06:36:18 -0500

    borrsr

---
+++ Pandas/1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+lista=pd.DataFrame({'A': [1, 2, 3]})
+print(lista)
+
+lista.name = "Lista_numeros"
+print(lista)
---
+++ Pandas/2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.DataFrame({
+    "columna1":[1,2,3],
+    "columna2":[1,2,3],
+    })
+
+print (df)
